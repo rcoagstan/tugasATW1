9f2aeaaf78e599e967a04dea6bdac4393296c9a4
rcoagstan <agustianr17@gmail.com>
2020-09-10 20:13:39 +0700

    Pembuatan class smarthphone

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-	echo "Holaa";
+class Smartphone{
+	public $merk, $tipe, $chipset, $ram, $resolusicamera, $harga;
+
+}
+
+
+$oneplus = new Smartphone;
+$oneplus->merk = "oneplus";
+$oneplus->tipe = "oneplus nord";
+$oneplus->chipset = "Snapdragon 765G";
+$oneplus->ram = "12GB";
+$oneplus->resolusicamera = "48MP";
+$oneplus->harga = "RP 5.900.000";
+
+echo "$bmw->merk";
