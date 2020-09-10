25ad6aabaebe84d6efb74840099f6af56e7fefb2
rcoagstan <agustianr17@gmail.com>
2020-09-10 21:23:35 +0700

    Class Smartphone

--- index.php
+++ index.php
@@ -14,4 +14,9 @@ $oneplus->ram = "12GB";
 $oneplus->resolusicamera = "48MP";
 $oneplus->harga = "RP 5.900.000";
 
-echo "$oneplus->merk";
+echo "$oneplus->merk";<br>
+echo "$oneplus->tipe";<br>
+echo "$oneplus->chipset";<br>
+echo "$oneplus->ram";<br>
+echo "$oneplus->resolusicamera";<br>
+echo "$oneplus->harga";<br>
